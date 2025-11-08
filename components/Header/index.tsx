8777d8f545bbfdc58c650e74b33fa65a24467f3b
mshabil <73349413+Mohamed-shabil@users.noreply.github.com>
2025-11-08 18:18:31 +0530

    feat: redesign home page and add header component

---
+++ components/Header/index.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import React from "react";
+import { Button } from "../ui/button";
+
+const Header = () => {
+    return (
+        <header className="max-w-[1200px] mx-auto flex border-b border-solid border-gray-200/80 px-10 py-4">
+            <div className="flex items-center">
+                <h1 className="text-2xl font-bold text-secondary-foreground">
+                    Temply
+                </h1>
+            </div>
+            <div className="flex-1 flex justify-end gap-8 items-center">
+                <Link
+                    href={"/features"}
+                    className="text-sm font-medium hover:text-primary"
+                >
+                    Features
+                </Link>
+                <Link
+                    href={"/features"}
+                    className="text-sm font-medium hover:text-primary"
+                >
+                    Pricing
+                </Link>
+                <div className="flex gap-2">
+                    <Button
+                        variant={"secondary"}
+                        className="text-sm font-semibold hover:text-primary tracking-[0.015em]"
+                    >
+                        Log in
+                    </Button>
+                    <Button
+                        variant={"default"}
+                        className="text-sm font-semibold tracking-[0.015em]"
+                    >
+                        Get Started
+                    </Button>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
