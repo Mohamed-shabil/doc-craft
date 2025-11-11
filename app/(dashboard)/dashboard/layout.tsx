ff3bc93b932a9c5f3684c6829e2d0821da5c28e4
mshabil <mohamedshabil231@gmail.com>
2025-11-12 00:23:17 +0530

    feat: refactor Header component and update layout structure in dashboard

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -1,13 +1,13 @@
-import Header from "@/components/landing-header";
+import Header from "@/components/header/index";
 export default function DashboardLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
-        <>
+        <main className="max-w-7xl mx-auto">
             <Header />
             {children}
-        </>
+        </main>
     );
 }
